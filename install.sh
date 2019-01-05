f666ae286454d06f11ee10f4c6d2855b62b05f3c
umbracoindevs <45613717+Umbracoindevs@users.noreply.github.com>
2019-01-05 02:45:44 -0500

    Revert "update?"
    This reverts commit fe63a7318430af7e0371fbb442e602509d2e95cd.

--- install.sh
+++ install.sh
@@ -12,11 +12,41 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-IP=$(curl -s4 icanhazip.com)
+IP==$(curl -s4 icanhazip.com)
 echo -e "${GREEN}Please enter your private key: (Copy from Windows and right click to paste and press enter)${NC}"
 read KEY
-echo -e "${GREEN}Completion: 0%...${NC}"
+sleep 2
+
+sudo apt-get update -y &>/dev/null
+sudo apt-get upgrade -y &>/dev/null
+echo -e "${GREEN}Completion: 5%...${NC}"
+sudo apt-get install libboost-all-dev libevent-dev software-properties-common -y &>/dev/null
+sudo add-apt-repository ppa:bitcoin/bitcoin -y &>/dev/null
+echo -e "${GREEN}Completion: 10%...${NC}"
+sudo apt-get update &>/dev/null
+echo -e "${GREEN}Completion: 20%...${NC}"
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y &>/dev/null
+echo -e "${GREEN}Completion: 30%...${NC}"
+sudo apt-get install libpthread-stubs0-dev -y &>/dev/null
+sudo apt-get install libzmq3-dev -y &>/dev/null
+echo -e "${GREEN}Completion: 40%...${NC}"
+sudo apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ unzip libzmq5 &>/dev/null
+echo -e "${GREEN}Completion: 75%...${NC}"
+
+sleep 2
+cd /var
+sudo touch swap.img &>/dev/null
+sudo chmod 600 swap.img &>/dev/null
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000 &>/dev/null
+sudo mkswap /var/swap.img &>/dev/null
+sudo swapon /var/swap.img &>/dev/null
+sudo free &>/dev/null
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab &>/dev/null
+echo -e "${GREEN}Completion: 85%...${NC}"
 cd
+sleep 2
 sudo apt-get install -y ufw &>/dev/null
 sudo ufw allow ssh/tcp &>/dev/null
 sudo ufw limit ssh/tcp &>/dev/null
@@ -24,8 +54,10 @@ sudo ufw allow 18878/tcp &>/dev/null
 sudo ufw logging on &>/dev/null
 echo "y" | sudo ufw enable &>/dev/null
 echo -e "${GREEN}Completion: 90%...${NC}"
-sudo chmod +x ./umbra*
-sudo cp ./umbrad ./umbra-cli /usr/local/bin
+sleep 2
+sudo chmod +x /root/umbramn/umbrad /root/umbramn/umbra-cli
+sudo mv /root/umbramn/umbrad /root/umbramn/umbra-cli /usr/local/bin
+sleep 2
 sudo mkdir /root/.umbra
 echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /root/.umbra/umbra.conf
 echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /root/.umbra/umbra.conf
